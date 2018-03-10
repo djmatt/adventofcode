a2977f1a9967582a889887657b87901f36bf3a02
djmatt <d01matt@gmail.com>
2018-03-09 20:43:07 -0500

    Adding Day 5 solution

---
+++ 2017/day05/aoc2017d05p2.py
@@ -0,0 +1,27 @@
+
+
+def jumper(instructions):
+    status = instructions
+    LO, HI = 0, len(status)
+    step = index = 0
+    while(LO <= index < HI):
+        jump_n = status[index]
+        status[index] += -1 if status[index] > 2 else 1
+        index += jump_n
+        step += 1
+    return step
+
+
+if __name__ == "__main__":
+    TESTCASES = [([0, 3, 0, 1, -3], 10)]
+
+    for input, expected in TESTCASES:
+        result = jumper(input)
+        assert(result == expected)
+
+    PUZZLEFILENAME = '.\\2017\\day05\\input.txt'
+    with open(PUZZLEFILENAME) as infile:
+        input = [int(x) for x in infile.readlines()]
+
+    result = jumper(input)
+    print(result)
